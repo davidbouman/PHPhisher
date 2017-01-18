d8e317796b496aec468b998ee9456bc7a88a7cf7
David Bouman <davidbouman35@gmail.com>
2017-01-18 21:38:50 +0100

    replaced JS with PHP and ommited closing tag
    optimized performance and better support
    
    read more about closing php tag: http://stackoverflow.com/questions/4410704/why-would-one-omit-the-close-tag#4499749

--- use.php
+++ use.php
@@ -3,5 +3,6 @@ $page = $_GET['page'];
 $j = json_decode(file_get_contents("config.json"), true);
 $j['page'] = $page;
 file_put_contents("config.json", json_encode($j));
+
 header("Location: index.php"); 
 

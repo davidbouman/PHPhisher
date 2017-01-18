16a5b70963b2b475b068dccb2dad26264685bbef
David Bouman <davidbouman35@gmail.com>
2017-01-18 21:36:06 +0100

    replaced JS with PHP and omitted closing php tag
    optimized performance and better support
    
    read more about closing php tag: http://stackoverflow.com/questions/4410704/why-would-one-omit-the-close-tag#4499749

--- reset.php
+++ reset.php
@@ -2,7 +2,5 @@
 $j = json_decode(file_get_contents("config.json"), true);
 $j['page'] = 0;
 file_put_contents("config.json", json_encode($j));
-echo '<script script type="text/javascript">';
-echo 'window.location.href ="index.php";'; 
-echo '</script>';
-?>
+header("Location: index.php"); 
+

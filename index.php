828bdd333f98b2d62a549e6195b62b4cb5676963
David Bouman <davidbouman35@gmail.com>
2017-01-18 21:30:15 +0100

    Optimized index.php
    Replaced javascript code with header() function for support for browsers without javascript and faster redirects.

--- index.php
+++ index.php
@@ -2,32 +2,21 @@
 $json = json_decode(file_get_contents("config.json"), true);
 switch ($json['page']) {
     case "1":
-        echo '<script script type="text/javascript">';
-		echo 'window.location.href ="pages/office365/index.html";'; 
-		echo '</script>';
+		header("Location: pages/office365/index.html"); 
 		break;
     case "2":
-        echo '<script script type="text/javascript">';
-		echo 'window.location.href ="pages/connection_reset/index.html";'; 
-		echo '</script>';
+		header("Location: pages/connection_reset/index.html"); 
 		break;
     case "3":
-        echo '<script script type="text/javascript">';
-		echo 'window.location.href ="pages/minimal/index.html";'; 
-		echo '</script>';
+		header("Location: pages/minimal/index.html"); 
 		break;
-	case "4":
-        echo '<script script type="text/javascript">';
-		echo 'window.location.href ="pages/dlink/";'; 
-		echo '</script>';
-	case "5":
-        echo '<script script type="text/javascript">';
-		echo 'window.location.href ="pages/oauth-login";'; 
-		echo '</script>';
+    case "4":
+		header("Location: pages/dlink/"); 
+		break;
+    case "5":
+		header("Location: pages/oauth-login");
 		break;
     default:
-        echo '<script script type="text/javascript">';
-		echo 'window.location.href ="install/";'; 
-		echo '</script>';
+		header("Location: install/");
 }
 ?>

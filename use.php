6779eb98561f2daea29ae6a524acc5a37beb20f6
David Bouman <davidbouman35@gmail.com>
2017-01-18 21:36:57 +0100

    optimize

--- use.php
+++ use.php
@@ -3,7 +3,5 @@ $page = $_GET['page'];
 $j = json_decode(file_get_contents("config.json"), true);
 $j['page'] = $page;
 file_put_contents("config.json", json_encode($j));
-echo '<script script type="text/javascript">';
-echo 'window.location.href ="index.php";'; 
-echo '</script>';
-?>
+header("Location: index.php"); 
+
